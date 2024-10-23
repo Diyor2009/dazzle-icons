0397364e7485813569bc5f9944175f08ad21bea5
Diyor Umarkulov <diyor.umarqulov009@gmail.com>
2024-10-23 12:37:14 +0500

    migrate: generated icons folder

--- scripts/generateIndex.js
+++ scripts/generateIndex.js
@@ -1,4 +1,4 @@
-import { existsSync, readdir, readdirSync, writeFileSync } from "fs";
+import { existsSync, readdirSync, writeFileSync, readFileSync } from "fs";
 import { fileURLToPath } from "url";
 import { join, dirname } from "path";
 
@@ -6,29 +6,35 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const indexPath = join(__dirname, "../src/index.ts");
-const iconsDir = join(__dirname, "../src");
+
+const iconsDir = join(__dirname, "../src/icons");
 
 if (!existsSync(iconsDir)) {
   console.error("Icons directory does not exist:", iconsDir);
   process.exit(1);
 }
 
-readdir(iconsDir, (err) => {
-  if (err) {
-    console.error("Error reading icons directory:", err);
-    return;
-  }
-});
-
 const iconFiles = readdirSync(iconsDir).filter((file) => file.endsWith(".tsx"));
+
+iconFiles.forEach((file) => {
+  const filePath = join(iconsDir, file);
+
+  let content = readFileSync(filePath, "utf-8");
+
+  content = content.replace(/stroke="[^"]*"/g, 'stroke="currentColor"');
+
+  writeFileSync(filePath, content, "utf-8");
+});
 
 const exports = iconFiles
   .map((file) => {
     const iconName = file.replace(".tsx", "");
-    return `export { default as ${iconName} } from './${iconName}.js';`;
+    return `export { default as ${iconName} } from './icons/${iconName}.js';`;
   })
   .join("\n");
 
 writeFileSync(indexPath, exports);
 
-console.log("index.ts has been generated successfully.");
+console.log(
+  "Icons have been processed and index.ts has been generated successfully."
+);
